80b80b23935fcbb4fbe6f5e2a35635cae005ebca
Shane Spoor <A00852406@ad.bcit.ca>
2016-03-29 22:53:22 -0700

    Well that was way easier than I was making it

--- Main.cpp
+++ Main.cpp
@@ -1,62 +1,39 @@
 #include <iostream>
 #include <vector>
 
-typedef struct match {
+struct match {
     int p1;
     int p2;
+};
 
-    bool operator==(const match& rhs) const {
-        return p1 == rhs.p1 && p2 == rhs.p2;
-    }
-} match;
+void RoundRobinWorker(std::vector<std::vector<match>> &matches, int n, int first_player) {
+	if (n == 2) {
+		// Base case: everybody plays their neighbour on day 0
+		match m = { first_player, first_player + 1 };
+		matches[0].push_back(m);
+	} else {
+		unsigned half = n / 2;
 
-void PlaceMatch(std::vector<std::vector<bool>> &days, std::vector<std::vector<match>>& matches, match& m) {
-    int day_num = 0;
-	// There's a constant time way to do day placement
-	// But I didn't do it
-    for (auto & day : days) {
-        // Each day has a corresponding set of players and list of matches
-        // The set contains which players have played that day (so that another day can be chosen if either player has already played)
-        if (!day[m.p1] && !day[m.p2]) {
-            day[m.p1] = true;
-            day[m.p2] = true;
-            matches[day_num].push_back(m);
-            break;
-        }
-        ++day_num;
-    }
-}
+		RoundRobinWorker(matches, half, first_player);
+		RoundRobinWorker(matches, half, first_player + half);
 
-void RoundRobinWorker(std::vector<std::vector<bool>> &days, std::vector<std::vector<match>> &matches, int n, int first_player) {
-    // Base case; make a match with these two players and insert it in the next available day
-    if (n == 2) {
-        match m = { first_player, first_player + 1 };
-        PlaceMatch(days, matches, m);
-    } else {
-        // Otherwise, subdivide and then have all players in one half play all in the other
-        int half = n >> 1;
-        RoundRobinWorker(days, matches, half, first_player);
-        RoundRobinWorker(days, matches, half, first_player + half);
+		for (unsigned i = 0; i < half; ++i) {
+			for (unsigned j = half; j < n; ++j) {
+				match m = { i + first_player, j + first_player };
 
-        for (int i = 0; i < half; i++) {
-            for (int j = half; j < n; j++) {
-                match m = { i + first_player, j + first_player };
-                PlaceMatch(days, matches, m);
-            }
-        }
-    }
+				unsigned day = ((i + j) % half) + half - 1;
+				matches[day].push_back(m);
+			}
+		}
+	}
 }
 
 // Precondition: k > 0
 std::vector<std::vector<match>> RoundRobin(int k) {
     int n = 1 << k;
 
-    std::vector<std::vector<bool>> days(n - 1);
-    for (auto & day : days) {
-        day.resize(n, false);
-    }
     std::vector<std::vector<match>> matches(n - 1);
-    RoundRobinWorker(days, matches, n, 0);
+    RoundRobinWorker(matches, n, 0);
     return matches;
 }
 
